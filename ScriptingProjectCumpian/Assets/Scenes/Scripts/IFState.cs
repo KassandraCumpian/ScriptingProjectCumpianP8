5a1235048f60a1d62444ab8d24f043af4010f980
Kassandra Cumpian <757140@student.dusd.net>
2023-11-03 15:25:44 -0700

    added if statements
    Today I added if statements

---
+++ ScriptingProjectCumpian/Assets/Scenes/Scripts/IFState.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class IFState : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float coffeeTemperature = 85.0f;
+        float hotLimitTemperature = 70.0f;
+        float coldLimitTemperature = 40.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space)) 
+
+        coffeeTemperature -= Time.deltaTime * 5f;
+    }
+
+    void TemperatureTest ()
+    {
+        // If coffee's temperature is greater than the hottest drinking temperature...
+        if (coffeeTemperature > hotLimitTemperature)
+        {
+            // ... do this.
+            print("Coffee is too hot.");
+        }
+        // If it isn't, but the coffee temperature is less than the coldest drinking temperature...
+        else if (coffeeTemperature < coldLimitTemperature)
+        {
+            // ... do this.
+            print("Coffee is too cold.");
+        }
+        // If it is neither of those then...
+        else
+        {
+            // ... do this.
+            print("Coffee is just right.");
+        }
+    }
+}
