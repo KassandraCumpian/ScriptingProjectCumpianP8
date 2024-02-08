8ac8cd2873215f6ee6c9c2e053a89933c70cc7ae
Kassandra Cumpian <757140@student.dusd.net>
2024-02-08 14:26:22 -0800

    Added new script
    Today I added a new script

---
+++ ScriptingProjectCumpian/Assets/Scripts/LinearInter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInter : MonoBehaviour
+{/*
+  //Inthis case, result = 4
+    float result = Mathf.Lerp (3f, 5f, 0.5f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+    }
+    //Here result = (4, 5, 6,)
+    Vector3 result = Vector3.Lerp (from, to, 0.75f);
+    */
+}
